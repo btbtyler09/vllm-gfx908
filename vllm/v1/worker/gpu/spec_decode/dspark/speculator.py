vllm-project/vllm

[Model Runner V2] Cache draft logits in model's LM head dtype (#50910)
Signed-off-by: Giancarlo Delfin <gdelfin@inferact.ai>

vllm/v1/worker/gpu/spec_decode/dspark/speculator.py
@@ -127,8 +127,8 @@ class DSparkSpeculator(DFlashSpeculator):
             self.seeds,
             sample_pos - 1,
             apply_temperature=True,
-            output_processed_logits=self.draft_logits,
-            output_processed_logits_col=self._step_cols[step],
+            logits_cache=self.draft_logits,
+            logits_cache_col=self._step_cols[step],
             use_fp64=self.use_fp64_gumbel,
         )
 

btbtyler09/vllm-gfx908

vllm/v1/worker/gpu/spec_decode/dspark/speculator.py
@@ -137,8 +137,8 @@ class DSparkSpeculator(DFlashSpeculator):
                     self.seeds,
                     sample_pos[:, i] - 1,
                     apply_temperature=True,
-                    output_processed_logits=self.draft_logits,
-                    output_processed_logits_col=self._step_cols[i],
+                    logits_cache=self.draft_logits,
+                    logits_cache_col=self._step_cols[i],
                     use_fp64=self.use_fp64_gumbel,
                 )
             else: